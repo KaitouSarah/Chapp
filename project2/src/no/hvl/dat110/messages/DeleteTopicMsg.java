4df53a88a5ede4d1c89d0f870b974ddb7e0792c8
SindreFonnes <sindre.fonnes@gmail.com>
2019-03-06 12:07:35 +0100

    Initial Commit

--- project2/src/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ project2/src/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -4,5 +4,44 @@ public class DeleteTopicMsg extends Message {
 	
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
-	
+    private MessageType type;
+    private String user;
+    private String topic;
+
+    public DeleteTopicMsg(String User, String Topic) {
+        this.type = MessageType.DELETETOPIC;
+        this.user = User;
+        this.topic = Topic;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    @Override
+    public MessageType getType() {
+        return type;
+    }
+
+    @Override
+    public String getUser() {
+        return user;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Message [type=" + type + ", user=" + user + ", topic=" + topic + "]";
+    }
 }
--- project2/src/no/hvl/dat110/messages/PublishMsg.java
+++ project2/src/no/hvl/dat110/messages/PublishMsg.java
@@ -4,5 +4,53 @@ public class PublishMsg extends Message {
 	
 	// TODO: 
 	// Implement objectvariables, constructor, get/set-methods, and toString method
-	
+    private MessageType type;
+    private String user;
+    private String topic;
+    private String content;
+
+    public PublishMsg(String User, String Topic, String content) {
+        this.type = MessageType.PUBLISH;
+        this.user = User;
+        this.topic = Topic;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    @Override
+    public MessageType getType() {
+        return type;
+    }
+
+    @Override
+    public String getUser() {
+        return user;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Message [type=" + type + ", user=" + user + ", topic=" + topic + ", content=" + content + "]";
+    }
 }
